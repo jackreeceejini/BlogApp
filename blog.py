873e2ed71606cb4fa7f7e38b503ef4fe8a99f2b4
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-09 12:48:39 -0400

    added bloghandler

--- blog.py
+++ blog.py
@@ -16,3 +16,12 @@ def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+class BlogHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))

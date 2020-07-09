03d8937f944c1fa91f2c2a8706bcd09a7384b652
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-09 13:01:55 -0400

    added handler for new post

--- blog.py
+++ blog.py
@@ -67,3 +67,19 @@ class PostPage(BlogHandler):
             return
 
         self.render("permalink.html", post = post)
+
+class NewPost(BlogHandler):
+    def get(self):
+        self.render("newpost.html")
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+        if subject and content:
+            p = Post(parent = blog_key(), subject = subject, content = content)
+            p.put()
+            self.redirect('/blog/%s' % str(p.key().id()))
+        else:
+            error = "subject and content, please!"
+            self.render("newpost.html", subject=subject, content=content, error=error)

8144e9d08d2f868c917bf1482d23f4f4cf5128e5
JACKREECE EJINI <jackreeceejini@gmail.com>
2020-07-09 12:41:26 -0400

    finished imports and template initialization

---
+++ blog.py
@@ -0,0 +1,14 @@
+import os
+import re
+from string import letters
+
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+                               
